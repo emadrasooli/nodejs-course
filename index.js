f243811b88f42cb8e262231c3e6ba6918167ed11
M.Emad Rasooli <rasooliemad2004@gmail.com>
2025-08-31 14:19:06 +0430

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import getPosts,{ getPostsLength } from './postController.js';
+
+console.log(getPosts());
+
+console.log(`Posts length: ${getPostsLength()}`);
+
+
+// const {generateRandomNumber , celciusToFahrenheit } = require('./utils');
+
+// const number = generateRandomNumber();
+// console.log(`Random number: ${number}`);
+// console.log(`celcius to Fahrenheit: ${celciusToFahrenheit(number)}`);
+
